43693ef95715b74fb5dbc88e8e09e916ad04819b
Brennan Fife <brenfife@gmail.com>
2021-02-08 16:38:57 -0600

    -

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { TVChartContainer } from './components/TVChartContainer/index';
+import TVChartContainer from './components/TVChartContainer/index';
 
 export default function App() {
   return <TVChartContainer />;
